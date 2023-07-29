ba64a0f73a1aea6d0d066da3d20d5777373c4f75
nakamina <n.mina10.0608@gmail.com>
2023-07-30 01:51:16 +0900

    update for more pretty site

--- app.vue
+++ app.vue
@@ -1,51 +1,5 @@
-<script setup>
-import { computed, ref } from 'vue';
-
-let id = 0
-
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([
-  { id: id++, text: 'タスク 1 ', done: false },
-  { id: id++, text: 'タスク 2 ', done: true },
-  { id: id++, text: 'タスク 3 ', done: false }
-])
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value
-    ? todos.value.filter((t) => !t.done)
-    : todos.value
-})
-
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
-}
-</script>
-
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
-    <button>新規タスクを作成</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">削除</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? '全てのタスクを確認' : '終わったタスクを隠す' }}
-  </button>
+  <div>
+    <Todo />
+  </div>
 </template>
-
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
---
+++ components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="grid h-screen place-items-center">
+        <h1 class="text-6xl">Test Component</h1>
+    </div>
+</template>
