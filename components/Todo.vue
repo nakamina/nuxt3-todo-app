deaab5c78ba4f768a7b6762b9c828ba9ea28eee8
nakamina <n.mina10.0608@gmail.com>
2023-08-22 22:41:16 +0900

    add comment

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="grid min-h-screen justify-center py-12">
         <div class="w-96 flex flex-col gap-2">
+            <!-- TodoFormの呼び出し -->
             <todo-form class="mb-12" />
+            <!-- TodoListの呼び出し -->
             <todo-list />
         </div>
     </div>
